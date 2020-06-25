429ec65cfecbef7942925c86abe9edd71283afd6
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-06-25 10:48:01 +0200

    Add pp Fix eslint

---
+++ js/oop.js
@@ -0,0 +1,25 @@
+class Validator {
+  static REQUIRED = 'REQUIRED'; //eslint-disable
+  static MIN_LENGTH = 'MIN_LENGTH';
+  static validate(value, flag, validatorValue) {
+
+  }
+}
+
+class User {}
+
+class UserInputForm {
+  constructor() {
+    this.form = document.getElementById('user-input');
+    this.userNameInput = document.getElementById('username');
+    this.passwordInput = document.getElementById('password');
+
+    this.form.addEventListener('submit', this.signupHandler.bind(this));
+  }
+
+  signupHandler(event) {
+    event.preventDefault();
+    const enteredUsername = this.userNameInput.value;
+    const enteredPassword = this.passwordInput.value;
+  }
+}

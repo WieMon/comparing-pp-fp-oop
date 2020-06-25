49b2409471cfdcc4811e2ecb3d598977e4e5db49
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-06-25 11:21:16 +0200

    Add oop

--- js/oop.js
+++ js/oop.js
@@ -1,12 +1,26 @@
 class Validator {
-  static REQUIRED = 'REQUIRED'; 
+  static REQUIRED = 'REQUIRED';
   static MIN_LENGTH = 'MIN_LENGTH';
   static validate(value, flag, validatorValue) {
-
+    if (flag === this.REQUIRED) {
+      return value.trim().length > 0;
+    }
+    if (flag === this.MIN_LENGTH) {
+      return value.trim().length > validatorValue;
+    }
   }
 }
 
-class User {}
+class User {
+  constructor(uName, uPassword) {
+    this.userName = uName;
+    this.userPassword = uPassword;
+  }
+
+  greet() {
+    console.log('Hi, I am ' + this.userName);
+  }
+}
 
 class UserInputForm {
   constructor() {
@@ -19,7 +33,20 @@ class UserInputForm {
 
   signupHandler(event) {
     event.preventDefault();
-    const enteredUsername = this.userNameInput.value;
+    const enteredUserName = this.userNameInput.value;
     const enteredPassword = this.passwordInput.value;
+
+    if (
+      !Validator.validate(enteredUserName, Validator.REQUIRED) ||
+      !Validator.validate(enteredPassword, Validator.MIN_LENGTH, 5)
+    ) {
+      alert('Invalid input - username or password is wrong (password should be at least six characters long).');
+      return;
+    }
+    const newUser = new User(enteredUserName, enteredPassword);
+    console.log(newUser);
+    newUser.greet();
   }
 }
+
+new UserInputForm();
